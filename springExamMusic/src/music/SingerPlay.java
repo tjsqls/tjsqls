5de6be8ca20b226a77ae76f5332f94ce7e4187c6
INEA <INEA@192.168.0.21>
2017-11-21 17:03:40 +0900

    luv

--- springExamMusic/src/music/SingerPlay.java
+++ springExamMusic/src/music/SingerPlay.java
@@ -5,5 +5,10 @@ public static void main(String[] args) {
 	System.out.println("안녕하세요~~");
 	System.out.println("임선빈 입니다~~~");
 	System.out.println("인애 확인");
+	
+	int 워 = 1;
+	int 너 = 0;
+	int 원 = 1;
+	
 }
 }
