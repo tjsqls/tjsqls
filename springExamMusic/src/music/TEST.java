6ac0019ec55325673f0bafe9469b2372bf4a9647
INEA <INEA@192.168.0.21>
2017-11-21 16:42:39 +0900

    새 클래스 하나 만들었어용 확인해주세요

---
+++ springExamMusic/src/music/TEST.java
@@ -0,0 +1,25 @@
+package music;
+
+public class TEST {
+
+	private int seonBin;
+	private int im;
+	private int babo;
+	
+	public TEST() {
+		seonBin = 1;
+		im = 2;
+		babo = 3;
+	}
+	
+	private boolean checkOurLeader() {
+		
+		if (seonBin+im == babo) {
+			System.out.println("인애 : ㅇㅈ");
+		}
+		return true;
+	}
+	
+	}
+
+
